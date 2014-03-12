25688152328915b8d8608bb9fd9be584e248e84a
Eric Liao <rcliao01@gmail.com>
2014-03-12 01:09:38 -0700

    find all bots in directory

--- manager.py
+++ manager.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 from subprocess import Popen, PIPE
+from os import listdir
 
-game = Popen(['java', 'game/BiddingGame'], stdin=PIPE, stdout=PIPE)
+botFiles = []
 
-game.stdin.write('Hello\n')
-game.stdin.flush()
-counter = game.stdout.readline()
+def initGame():
+	game = Popen(['java', 'game/BiddingGame'], stdin=PIPE, stdout=PIPE)
 
-print str(counter)
+	game.stdin.write('Hello\n')
+	game.stdin.flush()
+	counter = game.stdout.readline()
+
+	print str(counter)
+
+def initBots():
+	# find out all bot files in bots directory
+	for file in listdir("bots"):
+		botFiles.append(file)
+
+def main():
+	initBots()
+
+if __name__ == "__main__":
+	main()
