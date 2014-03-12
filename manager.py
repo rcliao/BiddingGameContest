a1cab726a2f9f5dd830bd9df0cb11465fba580d7
Eric Liao <rcliao01@gmail.com>
2014-03-12 01:59:14 -0700

    add auto compile system

--- manager.py
+++ manager.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python
 from subprocess import Popen, PIPE
 from os import listdir
+import random
 
 botFiles = []
 
 def initGame():
 	game = Popen(['java', 'game/BiddingGame'], stdin=PIPE, stdout=PIPE)
+	return game
 
-	game.stdin.write('Hello\n')
+def getState(game):
+	game.stdin.write('+state\n')
 	game.stdin.flush()
-	counter = game.stdout.readline()
+	state = game.stdout.readline()
 
-	print str(counter)
+	return state
 
-def initBots():
+def readBotFiles():
 	# find out all bot files in bots directory
 	for file in listdir("bots"):
 		botFiles.append(file)
 
+def randomPair():
+	randomNumbers = random.sample(range(0, len(botFiles)), 2)
+	bot1 = botFiles[randomNumbers[0]]
+	bot2 = botFiles[randomNumbers[1]]
+	return bot1, bot2
+
+def initBots(bot1, bot2):
+	autoCompile(bot1)
+	autoCompile(bot2)
+
+def autoCompile(filename):
+	if filename.endswith(".scala"):
+		pass
+	elif filename.endswith(".py"):
+		pass
+	elif filename.endswith(".java"):
+		Popen(['javac', 'bots/' + filename])
+
 def main():
-	initBots()
+	game = initGame()
+
+	readBotFiles()
+	bot1, bot2 = randomPair()
+	initBots(bot1, bot2)
+
+
 
 if __name__ == "__main__":
 	main()
